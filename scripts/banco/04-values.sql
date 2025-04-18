413e3ef10b66d7f6c09633167a2a34ed717f1a01
ItaloBrazucaDeveloper <italo.ter.pes@gmail.com>
2025-04-17 23:00:55 -0300

    feat: Adiciona dois triggers - 02-triggers - e duas views - 03-views
    - Cria um trigger - AumentaFavorito - para aumentar a quantidade de favoritos de cada PublicacaoServico
    - Cria um trigger - DiminuiFavorito - para diminuir a quantidade de favoritos de cada PublicacaoServico
    - Cria uma view - ViewUsuario - para facilitar a consulta de login
    - Cria uma view - ViewPublicacao - para facilitar a consulta de listar publicações de serviços

---
+++ scripts/banco/04-values.sql
@@ -0,0 +1,19 @@
+INSERT INTO CategoriaServico (Nome) VALUES
+    ('Babá'),
+    ('Limpeza Residencial'),
+    ('Jardinagem'),
+    ('Mudança/Frete'),
+    ('Manuntenção'),
+    ('Reforço Escolar'),
+    ('Cuidador de Idoso'),
+    ('Cuidado Pet'),
+    ('Cozinha'),
+    ('Costura'),
+    ('Fotógrafo'),
+    ('Esteticista');
+
+INSERT INTO TipoContato (Nome) VALUES
+    ('Whatsapp'),
+    ('Instagram'),
+    ('Telefone'),
+    ('Portifolio');
