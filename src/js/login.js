ebfa76b33e5d8b9800385de70a634f66f5e41004
leitielly <leitiellynana@hotmail.com>
2025-05-18 19:51:06 -0300

    add arquivos

---
+++ src/js/login.js
@@ -0,0 +1,19 @@
+
+
+// Função para alternar a visualização da senha
+function togglePasswordVisibility(idInput, idIcon) {
+    const input = document.getElementById(idInput);
+    const icon = document.getElementById(idIcon);
+  
+    if (input.type === "password") {
+      input.type = "text";
+      icon.classList.remove("fa-eye");
+      icon.classList.add("fa-eye-slash");
+    } else {
+      input.type = "password";
+      icon.classList.remove("fa-eye-slash");
+      icon.classList.add("fa-eye");
+    }
+  }
+
+  
