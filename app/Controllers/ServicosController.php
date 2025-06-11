0f23923524add90de519576e473c86d437f92bab
ItaloBrazucaDeveloper <italo.ter.pes@gmail.com>
2025-06-11 16:19:27 -0300

    refactor(servicos): Remove métodos e arquivos obsoletos, atualiza a estrutura das views e implementa novos componentes para a página de serviços

--- app/Controllers/ServicosController.php
+++ app/Controllers/ServicosController.php
@@ -15,28 +15,28 @@ use KissPhp\Attributes\Http\Request\RouteParam;
 
 #[Controller('/servicos')]
 class ServicosController extends WebController {
-  public function __construct(private ServicosService $service) { }
+  // public function __construct(private ServicosService $service) { }
   
   #[Get()]
   public function exibirPaginaDeServicos() {
-    $servicos = $this->service->buscarServicos();
+    // $servicos = $this->service->buscarServicos();
 
-    $this->render('Pages/servicos/page', [
-      'Servicos' => $servicos
+    $this->render('Pages/servicos/page.twig', [
+      'Servicos' => []
     ]);
   }
 
   #[Get('/postar-servico')]
-  public function exibirPaginaDePostarServico() {
-    $session = $this->session;
-    $ultimoServicoInserido = $session->get('UltimoServicoInserido');
+  public function exibirPaginaDePostarServico(Request $request) {
+    // $session = $request->session;
+    // $ultimoServicoInserido = $session->get('UltimoServicoInserido');
     
-    $categorias = $this->service->buscarCategorias();
+    // $categorias = $this->service->buscarCategorias();
 
-    $this->render('Pages/servicos/postar-servico-form', [
-      'Categorias' => $categorias,
-      'UltimoServicoInserido' => $ultimoServicoInserido
-    ]);
+    // $this->render('Pages/servicos/postar-servico-form', [
+    //   'Categorias' => $categorias,
+    //   'UltimoServicoInserido' => $ultimoServicoInserido
+    // ]);
   }
 
   #[Post('/postar-servico')]
@@ -44,28 +44,28 @@ class ServicosController extends WebController {
     Request $request,
     #[Body] CadastroServico $servico
   ) {
-    $foto = $request->body->get('foto');
-    $foiCadastrado = $this->service->cadastrarServico($servico, $foto);
+    // $foto = $request->getBody('foto');
+    // $foiCadastrado = $this->service->cadastrarServico($servico, $foto);
 
-    if ($foiCadastrado) return $this->redirect('/servicos');
+    // if ($foiCadastrado) return $this->redirect('/servicos');
 
-    $this->session->set('UltimoServicoInserido', $servico);
-    return $this->redirect('/servicos/postar-servico');
+    // $this->session->set('UltimoServicoInserido', $servico);
+    // return $this->redirect('/servicos/postar-servico');
   }
 
   #[Post('/desativar/{id}')]
   public function desativarServico(#[RouteParam] int $id) {
-    try {
-      $idUsuario = $this->session->get('id_usuario');
+    // try {
+    //   $idUsuario = $this->session->get('id_usuario');
       
-      if (!$idUsuario) {
-        return $this->redirect('/login');
-      }
+    //   if (!$idUsuario) {
+    //     return $this->redirect('/login');
+    //   }
 
-      $this->service->desativarServico($id, $idUsuario);
-      return $this->redirect('/prestadores?sucesso=1');
-    } catch (\Exception $e) {
-      return $this->redirect('/prestadores?erro=' . urlencode($e->getMessage()));
-    }
+    //   $this->service->desativarServico($id, $idUsuario);
+    //   return $this->redirect('/prestadores?sucesso=1');
+    // } catch (\Exception $e) {
+    //   return $this->redirect('/prestadores?erro=' . urlencode($e->getMessage()));
+    // }
   }
 }
